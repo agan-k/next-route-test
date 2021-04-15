7fb9e262ad2cd91e4816ca6e952ee9da8ec29a54
Koran Agan <koranagan@gmail.com>
2021-04-15 14:25:57 -0400

    render news_cards only

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,51 @@ import Layout from '../components/layout'
 export default function Home(props) {
 
    const content = props.content.results.map(result => result)
+
+   function formatPrismicDate(date) {
+      let months = ["January","February","March","April","May","June","July",
+         "August", "September", "October", "November", "December"];
+      let day;
+      let month;
+      let year = date.slice(0, 4);
+   
+      if (date.charAt(8) == '0') {
+         day = date.slice(9, 10);
+      }
+      else {
+         day = date.slice(8, 10);
+      }
+      if (date.charAt(5) == '0') {
+         month = months[date.slice(6, 7) - 1];
+      }
+      else {
+         month = months[date.slice(5, 7) -1]
+      }
+      return day + '. ' + month + ' ' + year;
+   }
+
+   let cards = props.content.results.filter(item => item.data.news_card)
+   const news_cards = cards.map((result) =>
+     <div key={result.uid} className={style.card}>
+            <Link href={`/${result.data.content_type.substr(0, 5)}/${result.uid}`} >
+               <a>
+                  <p className={style.date}>{formatPrismicDate(result.data.date)}</p>
+                  <img src={result.data.img.url}/>
+                  {RichText.render(result.data.news_card_blurb)}
+                  <span className={style.arrow}>&rarr;</span>
+               </a>
+            </Link>
+         </div>
+   ) 
   
    console.log(content)    
    return (
       <Layout>
          <div className={style.container}>
-            <h1>last commit -m "remove Home content"</h1>
-   
+            <h1>last commit -m "render news_cards only"</h1>
+            <div className={style.grid}>
+               {news_cards}
+            </div>
          </div>
       </Layout>
    )
