0ae363db9080936784e102a0bb1ba8a4aa42f7f2
Koran Agan <koranagan@gmail.com>
2021-04-15 17:27:03 -0400

    move formatPrismicDate outside of Post function

--- pages/blog/[uid].js
+++ pages/blog/[uid].js
@@ -10,29 +10,10 @@ import Modal from "../../components/modal"
 import style from "./post.module.css"
 
 export default function Post({ data }) {
-
+console.log(data.date)
    const [showModal, setShowModal] = useState(false)
 
-   function formatPrismicDate(date) {
-      let months = ["January","February","March","April","May","June","July",
-         "August", "September", "October", "November", "December"];
-      let day;
-      let month;
-      let year = date.slice(0, 4);
-      if (date.charAt(8) == '0') {
-         day = date.slice(9, 10);
-      }
-      else {
-         day = date.slice(8, 10);
-      }
-      if (date.charAt(5) == '0') {
-         month = months[date.slice(6, 7) - 1];
-      }
-      else {
-         month = months[date.slice(5, 7) -1]
-      }
-      return day + '. ' + month + ' ' + year;
-   }
+   
 
    return (
       <Layout>
@@ -58,6 +39,26 @@ export default function Post({ data }) {
          </div>
       </Layout>
    )
+}
+function formatPrismicDate(date) {
+   let months = ["January","February","March","April","May","June","July",
+      "August", "September", "October", "November", "December"];
+   let day;
+   let month;
+   let year = date.slice(0, 4);
+   if (date.charAt(8) == '0') {
+      day = date.slice(9, 10);
+   }
+   else {
+      day = date.slice(8, 10);
+   }
+   if (date.charAt(5) == '0') {
+      month = months[date.slice(6, 7) - 1];
+   }
+   else {
+      month = months[date.slice(5, 7) -1]
+   }
+   return day + '. ' + month + ' ' + year;
 }
 
 export async function getStaticProps({ params }) {
--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home(props) {
    
    return (
       <Layout>
-         <h1>last commit -m "bring back formatPrismicDate to blog/[uid]"</h1>
+         <h1>last commit -m "move formatPrismicDate outside of Post function"</h1>
          <div className={style.container}>
             
             <main className={style.main}>
