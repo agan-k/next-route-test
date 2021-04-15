b0fdf775c23a06a5d3043f5fe827d2227c314ab2
Koran Agan <koranagan@gmail.com>
2021-04-15 13:56:09 -0400

    import Home minus modal component

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,86 @@ import Prismic from "prismic-javascript"
 import { client } from "../prismic-configuration"
 import { RichText } from "prismic-reactjs"
 
-import style from './Home.module.css'
 import Layout from '../components/layout'
+// import Modal from '../components/modal'
+import style from './Home.module.css'
 
 export default function Home(props) {
-   const content = props.content.results.map(result => result)
-   // const test_content =  content.map(item =>
-   //    <div key={item.data.id}>
-   //       <Link href={`/${item.uid}`}>
-   //          <h1>{item.data.date}</h1>
-   //       </Link>
-   //    </div>
+
+   const [showModal, setShowModal] = useState(false)
+   const [videoURL, setVideoURL] = useState(null)
+
+   function formatPrismicDate(date) {
+      let months = ["January","February","March","April","May","June","July",
+         "August", "September", "October", "November", "December"];
+      let day;
+      let month;
+      let year = date.slice(0, 4);
+   
+      if (date.charAt(8) == '0') {
+         day = date.slice(9, 10);
+      }
+      else {
+         day = date.slice(8, 10);
+      }
+      if (date.charAt(5) == '0') {
+         month = months[date.slice(6, 7) - 1];
+      }
+      else {
+         month = months[date.slice(5, 7) -1]
+      }
+      return day + '. ' + month + ' ' + year;
+   }
       
-      // )
-   console.log(content)    
+   let cards = props.content.results.filter(item => item.data.news_card)
+   const news_cards = cards.map((result) =>
+      
+      result.data.news_card && result.data.content_type == 'video' ?
+         
+         <div key={result.uid} className={style.card}
+            onClick={() => setVideoURL(result.data.video_link[0].text)}
+         >
+            <a>
+               <p className={style.date}>{formatPrismicDate(result.data.date)}</p>
+               <img src={result.data.img.url} onClick={() => setShowModal(true)}/>
+               {RichText.render(result.data.news_card_blurb)}
+               <span className={style.arrow}>&rarr;</span>
+            </a>
+         </div> :
+
+      (result.data.news_card && result.data.content_type !== 'video') ?
+            
+         <div key={result.uid} className={style.card}>
+            <Link href={`/${result.data.content_type.substr(0, 5)}/${result.uid}`} >
+               <a>
+                  <p className={style.date}>{formatPrismicDate(result.data.date)}</p>
+                  <img src={result.data.img.url}/>
+                  {RichText.render(result.data.news_card_blurb)}
+                  <span className={style.arrow}>&rarr;</span>
+               </a>
+            </Link>
+         </div> : ''
+   )      
+
    return (
       <Layout>
+         <h1>last commit -m "import Home minus modal component"</h1>
          <div className={style.container}>
-            <h1>last commit -m "remove test_content and add bio page"</h1>
-            <Link href="/blog">
-               <h1>blog</h1>
-            </Link>
-            <h1>date</h1>
-            {/* {test_content} */}
-         </div>
+            
+            <main className={style.main}>
+               <img className={style.banner} src={'/images/home_banner.jpg'}/>
+               <div className={style.grid}>
+                  {news_cards}
+                </div>
+   
+               {/* {showModal && (
+                  <Modal
+                     news_card_video_url={videoURL}
+                     closeModal={() => setShowModal(false)}
+                  />
+               )} */}
+            </main>
+          </div>
       </Layout>
    )
 }
@@ -37,7 +93,10 @@ export default function Home(props) {
 export async function getStaticProps() {
    const content = await client.query(
       Prismic.Predicates.at("document.type", "content"),
-      {pageSize : 100}
+      {
+         orderings: '[my.content.date desc]',
+         pageSize : 100
+      }
    )
    return {
       props: {
