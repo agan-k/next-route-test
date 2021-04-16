208fbd90d21347a62deff377106b4be8a94fca8b
Koran Agan <koranagan@gmail.com>
2021-04-16 00:01:54 -0400

    add Album sans conditional render

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home(props) {
    
    return (
       <Layout>
-         <h1>last commit -m "remove Album"</h1>
+         <h1>last commit -m "add Album sans conditional render"</h1>
          <div className={style.container}>
             
             <main className={style.main}>
