b457ab48da576e3af775f623bbe4b72759e44bdf
Koran Agan <koranagan@gmail.com>
2021-04-15 23:13:07 -0400

    add /disco

---
+++ pages/disco/[uid].js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Link from 'next/link'
+
+import Prismic from "prismic-javascript"
+import { client } from "../../prismic-configuration"
+import { RichText } from "prismic-reactjs"
+
+import BandcampPlayer from 'react-bandcamp'
+import style from './album.module.css'
+import Layout from '../../components/layout'
+
+export default function Album({data}) {
+   return (
+      <Layout>
+         <div className={style.container}>
+            <h1>&larr;
+               <Link href='/disco'><a>full discography</a></Link>
+            </h1>
+            <main>
+               <div className={style.album_info}>
+                  <img src={data.img.url} style={{maxHeight: '10rem'}}/>
+                  {RichText.render(data.content_body)}
+               </div>
+               {data.bandcamp_id[0].text && (
+                  <BandcampPlayer
+                     className='audio-player'
+                     album={data.bandcamp_id[0].text}
+                     width='40%'
+                     height='460px'
+                     artwork='none'
+                  />
+               )}
+            </main>
+         </div>
+      </Layout>
+   )
+}
+
+export async function getStaticProps({ params }) {
+   const { uid } = params;
+   const { data } = await client.getByUID("content", uid);
+   return {
+      props: {
+         data
+      }
+   }
+}
+
+export async function getStaticPaths() {
+   const { results } = await client.query(
+      Prismic.Predicates.at("document.type", "content"),
+      { pageSize: 100 }
+   )
+   const paths = results.map(result => ({
+      params: {
+         uid: result.uid,
+      }
+   }))
+   return {
+      paths,
+      fallback: false
+   }
+}
--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home(props) {
    
    return (
       <Layout>
-         <h1>last commit -m "add /contact"</h1>
+         <h1>last commit -m "add /disco"</h1>
          <div className={style.container}>
             
             <main className={style.main}>
