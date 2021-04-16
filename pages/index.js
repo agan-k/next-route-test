c870119b9eb4c71a005a6b590a42ab1bb9ad4654
Koran Agan <koranagan@gmail.com>
2021-04-15 23:28:05 -0400

    remove Album

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home(props) {
    
    return (
       <Layout>
-         <h1>last commit -m "add /disco"</h1>
+         <h1>last commit -m "remove Album"</h1>
          <div className={style.container}>
             
             <main className={style.main}>
